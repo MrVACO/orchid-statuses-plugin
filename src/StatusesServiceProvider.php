3801f6b0d56a0cdba7727028738cd44168e05f90
Mr.VACO <brute1904@gmail.com>
2023-12-03 16:51:45 +0500

    Added publish migrations

--- src/StatusesServiceProvider.php
+++ src/StatusesServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace MrVaco\OrchidStatusesManager;
 
+use MrVaco\HelperCode\Classes\Migrations;
 use Orchid\Platform\Dashboard;
 use Orchid\Platform\OrchidServiceProvider;
 
@@ -12,10 +13,25 @@ class StatusesServiceProvider extends OrchidServiceProvider
     public function boot(Dashboard $dashboard): void
     {
         parent::boot($dashboard);
+
+        $this->publish();
     }
 
     public function menu(): array
     {
         return [];
+    }
+
+    protected function publish()
+    {
+        if (!$this->app->runningInConsole())
+        {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__ . '/../stubs/create_statuses_table.stub'      => Migrations::getMigrationFileName('create_statuses_table.php'),
+            __DIR__ . '/../stubs/create_statuses_list_table.stub' => Migrations::getMigrationFileName('create_statuses_list_table.php'),
+        ], 'migrations');
     }
 }
