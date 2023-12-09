1513cff6407235ffe04aa52df6b0ddbeaca5cf9e
Mr.VACO <brute1904@gmail.com>
2023-12-10 02:30:33 +0500

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -26,29 +26,40 @@ app('router')
                     );
 
                 app('router')
-                    ->screen('create', EditScreen::class)
                     ->name('create')
-                    ->breadcrumbs(fn (Trail $trail) => $trail
-                        ->parent('platform.index')
-                        ->push(__('Statuses'), route(sprintf('%s.status.list', StatusClass::$plugin_prefix)))
-                        ->push(__('Create status'))
-                    );
+                    ->group(static function()
+                    {
+                        app('router')
+                            ->screen('create', EditScreen::class)
+                            ->name('')
+                            ->breadcrumbs(fn (Trail $trail) => $trail
+                                ->parent('platform.index')
+                                ->push(__('Statuses'), route(sprintf('%s.status.list', StatusClass::$plugin_prefix)))
+                                ->push(__('Create status'))
+                            );
+                    });
 
                 app('router')
-                    ->screen('create/save', EditScreen::class)
-                    ->name('create.save');
-
-                app('router')
-                    ->screen('{status}/edit', EditScreen::class)
                     ->name('edit')
-                    ->breadcrumbs(fn (Trail $trail, $status) => $trail
-                        ->parent('platform.index')
-                        ->push(__('Statuses'), route(sprintf('%s.status.list', StatusClass::$plugin_prefix)))
-                        ->push(__('Edit status'), route(sprintf('%s.status.edit', StatusClass::$plugin_prefix), $status))
-                    );
+                    ->prefix('edit')
+                    ->group(static function()
+                    {
+                        app('router')
+                            ->screen('{status}/edit', EditScreen::class)
+                            ->name('')
+                            ->breadcrumbs(fn (Trail $trail, $status) => $trail
+                                ->parent('platform.index')
+                                ->push(__('Statuses'), route(sprintf('%s.status.list', StatusClass::$plugin_prefix)))
+                                ->push(__('Edit status'), route(sprintf('%s.status.edit', StatusClass::$plugin_prefix), $status))
+                            );
+                    });
 
                 app('router')
-                    ->screen('{status}/edit/remove', EditScreen::class)
+                    ->screen('save', EditScreen::class)
+                    ->name('save');
+
+                app('router')
+                    ->screen('{status}/remove', EditScreen::class)
                     ->name('remove');
             });
     });
