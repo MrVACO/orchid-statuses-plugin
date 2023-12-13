0cb3bfd7c70cfb2ca8a13a0c9ca911d72bac0afb
Mr.VACO <brute1904@gmail.com>
2023-12-13 23:20:10 +0500

    Updated tag "publishes"

--- src/StatusesServiceProvider.php
+++ src/StatusesServiceProvider.php
@@ -72,6 +72,6 @@ class StatusesServiceProvider extends OrchidServiceProvider
             __DIR__ . '/../stubs/create_statuses_table.stub'        => Migrations::getMigrationFileName('create_statuses_table.php'),
             __DIR__ . '/../stubs/create_statuses_groups_table.stub' => Migrations::getMigrationFileName('create_statuses_groups_table.php'),
             __DIR__ . '/../stubs/fill_statuses.stub'                => Migrations::getMigrationFileName('fill_statuses.php'),
-        ], 'migrations');
+        ], 'plugin-migrations');
     }
 }
